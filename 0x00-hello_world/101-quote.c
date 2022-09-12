da3f66e09dcbd1ffa5814a7e07e9dd61c7f90273
Leoluwa <leoluamid@gmail.com>
2022-09-12 05:34:15 -0700

    Printing to stderr without printf, or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/*
+ * Using the fprintf() function :)
+ *
+ */
+#include "stdio.h"
+/**
+ * main - Entyry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (0);
+}
