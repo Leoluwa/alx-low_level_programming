419d0b665d6d1544f2a345bbc640ddb24aa05feb
Leoluwa <leoluamid@gmail.com>
2022-09-12 02:40:26 -0700

    Print a line as is :)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/*
+ * This program prints a line if text exactly as it is followed by a new line.
+ *
+ */
+#include "stdio.h"
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,");
+
+	return(0);
+}
