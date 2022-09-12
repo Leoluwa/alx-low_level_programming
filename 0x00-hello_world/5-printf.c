7ace3353f9c0223989703e206f847c3b13506fba
Leoluwa <leoluamid@gmail.com>
2022-09-12 03:12:29 -0700

    Using the printf() function :)

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ * Using the printf() function :)
+ *
+ */
+#include "stdio.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, ut the outcome is a piece of art,\n");
+
+	return (0);
+}
