f8215f6c71eb46cbf99b1f7841915eff402d8cce
Leoluwa <leoluamid@gmail.com>
2022-09-12 03:04:29 -0700

    i left out "

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -13,7 +13,7 @@ int main(void)
 	/*
 	 * To print the statement and a new line.
 	 */
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
