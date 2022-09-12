8790b1018f433aac125d6358094f9cbbc086be6c
Leoluwa <leoluamid@gmail.com>
2022-09-12 07:11:18 -0700

    The write() function or system call is interesting to learn about :)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 /*
- * Using the fprintf() function :)
+ * Using the write() system call / function :)
  *
  */
 #include "stdio.h"
@@ -11,7 +11,7 @@
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
