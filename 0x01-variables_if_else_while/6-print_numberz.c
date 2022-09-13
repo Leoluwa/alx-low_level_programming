d1ee5b1cbad2b5600f245591776a1f7adb4ad555
Leoluwa <leoluamid@gmail.com>
2022-09-12 21:01:27 -0700

    now

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	short Count;
+
+	for (Count = 48; Count <= 57; Count++)
+	{
+		putchar(Count);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
