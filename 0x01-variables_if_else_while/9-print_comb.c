6165f0549c123e886eccfc49f2eb66e2016f64ae
Leoluwa <leoluamid@gmail.com>
2022-09-12 21:23:54 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	short Count;
+
+	for (Count = 48; Count <= 57; Count++)
+	{
+		putchar(Count);
+
+		if (Count < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
