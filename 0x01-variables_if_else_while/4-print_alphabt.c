54657b0f2f815e513122da6cc5ccd0df0c482850
Leoluwa <leoluamid@gmail.com>
2022-09-12 20:46:54 -0700

    betty correct

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,6 @@
-/*
- * I am at peace just for the sake of it :)
- *
- * Abdulwahab
- *
- */
 #include "stdio.h"
 /**
  * main - Entry point
- *
- *
  *
  * Return: Always 0 (Success)
  */
@@ -16,19 +8,6 @@ int main(void)
 {
 	short Count;
 
-	/*
-	 * ASCII code :
-	 *
-	 * For lowercase, a is 97, zis 122, e is 101, q is 113
-	 *
-	 */
-
-	/*
-	 *
-	 * Print the alphabets in lowercase, except e and q,
-	 * then followed by new line
-	 *
-	 */
 	for (Count = 97; Count <= 122; Count++)
 	{
 		if ((Count != 101) && (Count != 113))
@@ -38,7 +17,7 @@ int main(void)
 	}
 
 	putchar('\n');
-	
-	
+
+
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/lphattt.c
@@ -0,0 +1,44 @@
+/*
+ * I am at peace just for the sake of it :)
+ *
+ * Abdulwahab
+ *
+ */
+#include "stdio.h"
+/**
+ * main - Entry point
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	short Count;
+
+	/*
+	 * ASCII code :
+	 *
+	 * For lowercase, a is 97, zis 122, e is 101, q is 113
+	 *
+	 */
+
+	/*
+	 *
+	 * Print the alphabets in lowercase, except e and q,
+	 * then followed by new line
+	 *
+	 */
+	for (Count = 97; Count <= 122; Count++)
+	{
+		if ((Count != 101) && (Count != 113))
+		{
+			putchar(Count);
+		}
+	}
+
+	putchar('\n');
+	
+	
+	return (0);
+}
