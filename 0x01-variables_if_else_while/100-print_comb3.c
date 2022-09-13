8313596963a76bb839afa4e240af28006e00ca51
Leoluwa <leoluamid@gmail.com>
2022-09-12 21:39:44 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (Success)
+ */
+ int main(void)
+ {
+	 int c = 0;
+	 int d;
+	 int e;
+
+	 while (c <= 99)
+	 {
+		 d = (c / 10 + '0');
+		 e = (c % 10 + '0');
+
+		 if (d < e)
+		 {
+			 putchar(d);
+			 putchar(e);
+			 if (c != 89)
+			 {
+				 putchar(',');
+				 putchar(' ');
+			 }
+		 }
+		 c++;
+	 }
+
+	 putchar('\n');
+
+	 return (0);
+ }
