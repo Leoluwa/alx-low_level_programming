1c81b675ba4885e668c6e9ffdace7e45be7aad19
Leoluwa <leoluamid@gmail.com>
2022-09-19 10:44:22 +0100

    Multiplication tables are interesting :)

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,80 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the times table of the given argument.
+ * @u: the given argument
+ *
+ * Return: Nothing.
+ *
+ */
+
+void print_times_table(int u)
+{
+	if ((u < 0) || (u > 15))
+	{
+		;
+	}
+	else
+	{
+		int l;
+		int m;
+		int n;
+
+		for (m = 0; m <= u; m++)
+		{
+			for (n = 0; n <= u; n++)
+			{
+				l = (m * n);
+
+				if ((l / 10) == 0)
+				{
+					if (n == 0)
+					{
+						_putchar('0');
+					}
+					if (n != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((l % 10) + '0');
+					}
+					if (n < u)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				else if ((l / 10) != 0)
+				{
+					if ((l / 10) > 9)
+					{
+						_putchar((l / 100) + '0');
+
+						_putchar(((l % 100) / 10) + '0');
+
+						_putchar(((l % 100) % 10) + '0');
+						if (n < u)
+						{
+							_putchar(',');
+							_putchar(' ');
+						}
+					}
+					else
+					{
+						_putchar(' ');
+						_putchar((l / 10) + '0');
+
+						_putchar((l % 10) + '0');
+						if (n < u)
+						{
+							_putchar(',');
+							_putchar(' ');
+						}
+					}
+				}
+			}
+
+			_putchar('\n');
+		}
+	}
+}
