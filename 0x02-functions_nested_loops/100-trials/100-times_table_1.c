5d4be49e015e35ceddd18e4efc2444e5425304d4
Leoluwa <leoluamid@gmail.com>
2022-09-19 13:17:54 +0100

    working on it

--- 0x02-functions_nested_loops/100-trials/100-times_table_1.c
+++ 0x02-functions_nested_loops/100-trials/100-times_table_1.c
@@ -11,14 +11,14 @@ void print_times_table(int u)
 	int l;
 	int m;
 	int n;
-		for (m = 0; (((u >= 0) && (u <= 15)) && (m <= u)); m++)
+		for (m = 0; (((u > 0) && (u <= 15)) && (m <= u)); m++)
 		{
 			for (n = 0; n <= u; n++)
 			{
 				l = (m * n);
 				if ((l / 10) == 0)
 				{
-					if (n == 0)
+					if (u == 0)
 					{
 						_putchar('0');
 					}
