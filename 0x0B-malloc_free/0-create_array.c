31525642ffe32536c1e0ed84322fb3175bf7879e
Motsoane <thopemotsoane@gmail.com>
2022-10-03 15:57:06 +0100

    Memory Allocation :)

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates a char array and initializes it with a specific character.
+ * create_array - creates a char array and initializes it
+ *		  with a specific character.
+ *
  * @size: Size of the array.
  * @c: Initial chartarcter.
  *
@@ -14,6 +16,11 @@ char *create_array(unsigned int size, char c)
 {
 	size_t u;
 	char *ptr = (char *)malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 
 	if (size == 0)
 	{
