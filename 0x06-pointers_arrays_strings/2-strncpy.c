5cb11290408a7e6125e8d521170387613d22126f
Leoluwa <leoluamid@gmail.com>
2022-10-16 23:35:42 +0100

    Pointer, Arrays, And Strings In C :)

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,54 +1,30 @@
 #include "main.h"
-
 /**
- * _strncpy - Copies n bytes from a string
- * @src: The string to copy from
- * @dest: The location to copy
- * @n: Number of bytes to copy
- *
- * Return: Pointer to dest
- *
+ *_strncpy - copies a string
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
+
 {
-	int l = 0;
-	int i = 0;
+int i;
 
-	while (*(src + l) != '\0')
-	{
-		l++;
-	}
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
 
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
 
+return (dest);
 
-	if (l < n)
-	{
-		for (i = 0; i < l; i++)
-		{
-			*(dest + i) = *(src + i);
-		}
-
-		for ( ; l < 90; l++)
-		{
-			*(dest + l) = '\0';
-		}
-
-		for ( ; l < 97; l++)
-		{
-			*(dest + l) = '*';
-		}
-
-		*(dest + (n - 1)) = '\0';
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			*(dest + i) = *(src + i);
-		}
-	}
-
-
-	return (dest);
 }
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,47 +1,36 @@
 #include "main.h"
-#define	Alph	26
-#define Abet	13
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: pointer to the argument string.
- * Return: Pointer to the modified string.
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int u;
-	int k;
-	int i;
-	int l;
-	int m;
-	char lower[Alph];
-	char upper[Alph];
-	char Separator[Abet] = {'\t', '\n', ' '};
-	char *Separats = ",.;!?(){}";
+	int count;
 
-	for (m = 0, l = 3; ((l < Abet) && (m < 9)); m++, l++)
-	{
-		Separator[l] = *(Separats + m);
-	}
-	l = 0;
-	while (*(str + l) != '\0')
-	{
-		l++; }
-	for (i = 65, u = 0; i <= 90; i++, u++)
-	{
-		upper[u] = i; }
-	for (k = 97, u = 0; k <= 122; k++, u++)
-	{
-		lower[u] = k; }
-	for (u = 1; u < l; u++)
-	{
-		for (i = 0; i < 10; i++)
+/*  scan through string */
+	count = 0;
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-			if (*(str + u) == *(Separator + i))
+			s[0] = s[0] - 32;
+		}
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
 			{
-				for (k = 0; k < 26; k++)
-				{
-					if (*(str + (u + 1)) == lower[k])
-					{
-						*(str + (u + 1)) = upper[k];
-					} } } } }
-	return (str); }
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
